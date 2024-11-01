a77af547746d9f9677b77caacf41c0b963401705
regarry <regarry@ncsu.edu>
2024-11-01 13:47:49 -0400

    Added exclusive GPU use and A10 node request to train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #BSUB -n 1
-#BSUB -W 24:00
+#BSUB -W 2:00
 #BSUB -q gpu
-#BSUB -gpu "num=1:mode=shared:mps=no"
+#BSUB -R "select[a10]"
+#BSUB -gpu "num=1:mode=exclusive_process:mps=no"
 #BSUB -o ./logs/%J.out
 #BSUB -e ./logs/%J.out
 source ~/.bashrc
+source /usr/share/Modules/init/bash
 module load cuda/11.0
 conda activate /usr/local/usrapps/$GROUP/$USER/unet # enter the full path to the conda environment
 #conda activate /rsstu/users/t/tghashg/MADMbrains/Ryan/conda/unet 
